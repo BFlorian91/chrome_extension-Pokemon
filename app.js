065c8df2b4d2212193d033191f2534beda665c35
FBeaumont <flbeaumo@student.42.fr>
2019-02-21 19:58:18 +0100

    test_chrome_extension

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var images = document.querySelectorAll('img');
 for (var i = 0; i < images.length; i++)
 {
-	images[i].src = 'http://aux.iconspalace.com/uploads/2116404522463441715.png'
+	images[i].src = 'https://media1.giphy.com/media/2aKCnMYAkaAS5AvQQ2/giphy.gif?cid=3640f6095c6e888d4433647a3214f3ec'
 }
